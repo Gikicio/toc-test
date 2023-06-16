0eff48d851be5d509f26174b9ca05c599927f6d3
Gikicio <84517952+Gikicio@users.noreply.github.com>
2023-06-16 08:19:30 +0200

    Update useTableOfContents.ts

--- src/components/TableOfContents/useTableOfContents.ts
+++ src/components/TableOfContents/useTableOfContents.ts
@@ -1,13 +1,41 @@
+import { useState, useCallback, useMemo } from 'react';
 import { ContentItem } from './types/ContentItem';
+import { TableOfContentsArg } from './types/ContentItem';
 
-type TableOfContentsArg = {
-    items: ContentItem[];
+const buildParentChildrenMap = (items: ContentItem[]) => {
+    const map: Record<string, ContentItem[]> = {};
+    items.forEach((item: ContentItem) => {
+        const parentId = item.parentId || 'root';
+        map[parentId] = map[parentId] || [];
+        map[parentId].push(item);
+    });
+    return map;
 };
 
-// TODO: expand/collapse of items
-// TODO: Collapse recursively
 export const useTableOfContents = ({ items }: TableOfContentsArg) => {
-    const onClick = (item: ContentItem) => () => console.log(item);
+    const [expanded, setExpanded] = useState<Set<string>>(new Set());
 
-    return { items, onClick };
+    const toggleExpand = useCallback((itemId: string) => {
+        setExpanded((prev) => {
+            const newSet = new Set(prev);
+            if (newSet.has(itemId)) {
+                newSet.delete(itemId);
+            } else {
+                newSet.add(itemId);
+            }
+            return newSet;
+        });
+    }, []);
+
+    const expandAll = useCallback(() => {
+        setExpanded(new Set(items.map(item => item.id)));
+    }, [items]);
+
+    const closeAll = useCallback(() => {
+        setExpanded(new Set());
+    }, []);
+
+    const childrenMap = useMemo(() => buildParentChildrenMap(items), [items]);
+
+    return { items, expanded, toggleExpand, expandAll, closeAll, childrenMap };
 };
