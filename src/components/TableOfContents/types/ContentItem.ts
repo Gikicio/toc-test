1a799eccba25bb6b2b5e86aa46c42a16f52accee
Gikicio <84517952+Gikicio@users.noreply.github.com>
2023-06-16 08:20:14 +0200

    Update ContentItem.ts

--- src/components/TableOfContents/types/ContentItem.ts
+++ src/components/TableOfContents/types/ContentItem.ts
@@ -1,8 +1,11 @@
-//? Given data type
 export type ContentItem = {
     id: string;
     level: number;
     name: string;
     parentId?: string | null;
     __typename?: string;
+};
+
+export type TableOfContentsArg = {
+    items: ContentItem[];
 };
